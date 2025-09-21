0fffdb8ab507558eb911fa10cb79dd0b6815341f
genbtcdoubler <79389495+genbtcdoubler@users.noreply.github.com>
2025-09-21 23:36:51 +0700

    Update truffle-config.js

--- Truffle script/truffle-config.js
+++ Truffle script/truffle-config.js
@@ -11,9 +11,8 @@ module.exports = {
           "https://mainnet.base.org"
         ),
       network_id: 8453,
-      gas: 21000,                    // standard ETH transfer
-      gasPrice: 1000000000           // 1 gwei (small but usually safe)
-      // gasPrice: 100000000          // 0.1 gwei (super small, may get stuck)
+      gas: 21000,                   
+      gasPrice: 500000000          
     }
   },
 
