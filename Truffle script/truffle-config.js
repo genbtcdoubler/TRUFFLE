791da32136c7ec62031053b0c30bc497d1479e95
genbtcdoubler <79389495+genbtcdoubler@users.noreply.github.com>
2025-09-21 23:33:26 +0700

    Update truffle-config.js

--- Truffle script/truffle-config.js
+++ Truffle script/truffle-config.js
@@ -3,17 +3,17 @@ require("dotenv").config();
 
 module.exports = {
   networks: {
-
     // Base Mainnet
     base: {
       provider: () =>
         new HDWalletProvider(
           process.env.PRIVATE_KEY,
-          "https://mainnet.base.org" // official RPC for Base mainnet
+          "https://mainnet.base.org"
         ),
       network_id: 8453,
-      gas: 21000,
-      gasPrice: 21000
+      gas: 21000,                    // standard ETH transfer
+      gasPrice: 1000000000           // 1 gwei (small but usually safe)
+      // gasPrice: 100000000          // 0.1 gwei (super small, may get stuck)
     }
   },
 
